9f36a19744ef5975f76ccab9b05b971de356e28d
Vitor-C-Souza <vitor_souza97@hotmail.com>
2024-03-19 17:41:57 -0300

    Inicio do projeto Screen Sound Music

---
+++ src/main/java/br/com/desafio/screensoundmusic/ScreenSoundMusicApplication.java
@@ -0,0 +1,19 @@
+package br.com.desafio.screensoundmusic;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenSoundMusicApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenSoundMusicApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+
+}
---
+++ src/test/java/br/com/desafio/screensoundmusic/ScreenSoundMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafio.screensoundmusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenSoundMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
