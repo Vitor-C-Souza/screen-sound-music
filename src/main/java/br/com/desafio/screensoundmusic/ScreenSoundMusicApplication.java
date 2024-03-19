bbaa402e7513615b570a724ba0c75dce99ef1026
Vitor-C-Souza <vitor_souza97@hotmail.com>
2024-03-19 20:05:57 -0300

    Configurando o layout inicial do projeto para iniciar.

--- src/main/java/br/com/desafio/screensoundmusic/ScreenSoundMusicApplication.java
+++ src/main/java/br/com/desafio/screensoundmusic/ScreenSoundMusicApplication.java
@@ -1,5 +1,7 @@
 package br.com.desafio.screensoundmusic;
 
+import java.util.Scanner;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,7 +15,40 @@ public class ScreenSoundMusicApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		Scanner read = new Scanner(System.in);
+
+		
+		boolean validacao;
+		do{
+			validacao = true;
+			System.out.println("""
+				***Screen Sound Music
+	
+				1- Cadastrar artistas
+
+				0- Sair
+			""");
+			var opcao = Integer.parseInt(read.nextLine());
+	
+			switch (opcao) {
+				case 1:
+					System.out.println("***CADASTRANDO ARTISTAS***");					
+					break;
+
+				case 0:
+					System.out.println("Finalizando o programa");
+					break;
+			
+				default:
+					System.out.println("Opção invalida!!!");
+					validacao = false;
+					break;
+			}
+		}while(validacao == false);
+
+
+		read.close();
+
 
 	}
-
 }
